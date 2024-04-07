da3b0a3b159a55bca267eaa2addff8c2b4f606f7
ristoarak <risto.arak@tktk.ee>
2024-04-07 09:51:47 +0300

    add Week3IfAndSwitch code

---
+++ IfDayOfTheWeek/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+
+if (weekDay == 0)
+{
+    Console.WriteLine("Its Sunday");
+}
+else if (weekDay == 1)
+{
+    Console.WriteLine("Its Monday");
+}
+else if (weekDay == 2)
+{
+    Console.WriteLine("Its Tuesday");
+}
+else if (weekDay == 3)
+{
+    Console.WriteLine("Its Wednesday");
+}
+else if (weekDay == 4)
+{
+    Console.WriteLine("Its Thursday");
+}
+else if (weekDay == 5)
+{
+    Console.WriteLine("Its Friday");
+}
+else
+{
+    Console.WriteLine("Its Saturday");
+}
+
+Console.WriteLine("Have nice day!");
---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,30 @@
+﻿int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);//0-6
+
+switch(weekDay) // väärtus midakontrollitakse
+{
+    case 0: // Case 0 on tegelikult  == if( weekDay  == 0)
+        Console.WriteLine(" Its Sunday");
+        break;
+        case 1:
+        Console.WriteLine("Its Monday");
+        break;
+    case 2:
+        Console.WriteLine("Its Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Its Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Its Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Its Friday");
+        break;
+    case 6:
+        Console.WriteLine("Its Saturday");
+        break;
+    default:
+        Console.WriteLine("Oops.your calender must be broken!");
+        break;
+}
+Console.WriteLine("Have a niceday!");
